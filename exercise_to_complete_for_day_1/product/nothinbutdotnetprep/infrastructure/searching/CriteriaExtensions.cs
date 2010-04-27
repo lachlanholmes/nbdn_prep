f6a23f9e930792dccd2b9d73255b9e76edbef6f7
Lachlan Holmes <lachlanholmes@gmail.com>
2010-04-26 19:49:12 -0600

    Added delegate searching

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/infrastructure/searching/CriteriaExtensions.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/infrastructure/searching/CriteriaExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetprep.infrastructure.searching
 {
@@ -19,5 +20,18 @@ namespace nothinbutdotnetprep.infrastructure.searching
         {
             return new AnonymousCriteria<T>(t => accessor(t).Equals(value_to_match)); 
         }
-    }
+
+				public static Criteria<T> not_equal_to<T, PropertyType>(this Func<T, PropertyType> accessor,
+																													PropertyType value_to_match)
+				{
+					return new AnonymousCriteria<T>(someVal => !accessor(someVal).Equals(value_to_match));
+				}
+
+				public static Criteria<T> equal_to_any<T, PropertyType>(this Func<T, PropertyType> accessor,
+																													params PropertyType[] values_to_match)
+				{
+					List<PropertyType> valuesList = new List<PropertyType>(values_to_match);
+					return new AnonymousCriteria<T>(someVal => valuesList.Contains(accessor(someVal)));
+				}
+}
 }
